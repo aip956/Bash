dd42c69cb634505063c9bf47667ab0883dedc6ce
Anthea Ip <antheaip964@gmail.com>
2024-06-29 10:54:31 -0700

    Added source to Ch1 file

--- AdventureQuest/Ch1MissingFile.sh
+++ AdventureQuest/Ch1MissingFile.sh
@@ -1,3 +1,6 @@
+# (https://github.com/John-Jepsen/labs/blob/main/bash_scripting.md)
+# https://docs.google.com/document/d/1T2lk7HDFrpUIZBJI7nJ9YVw6KH3RSaPF-zR2vAwoAI8/edit
+
 # File Existence Check Script
 
 echo "Enter the filename to check:"
